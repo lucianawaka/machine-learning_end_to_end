191e128d1f8fd58e52c796dac06def38fbd049cf
Luciana Bahia Wakabayashi Fabri <lucianawakabayashi@gmail.com>
2022-09-24 17:30:53 -0300

    params corretos

--- Aplicacao.py
+++ Aplicacao.py
@@ -6,21 +6,27 @@ import joblib
 Aplicativo = Flask(__name__)
 
 # Carregar o modelo treinado
-modelo = joblib.load('modelo/Modelo_Floresta_Aleatoria_v1.pkl')
+modelo = joblib.load('Modelo_Floresta_Aleatoria_v1.pkl')
 
 # Função para receber nossa API
 @Aplicativo.route('/API_Preditivo/<area>;<rooms>;<bathroom>;<parking_spaces>;<floor>;<animal>;<furniture>;<hoa>;<property_tax>', methods=['GET'])
- 
-def Funcao_01( area,rooms,bathroom,parking_spaces,floor,animal,furniture,hoa,property_tax):
+#@Aplicativo.route('/API_Preditivo', methods=['GET'])
+#def Funcao_01( area, rooms, bathroom, parking_spaces, floor, animal, furniture, hoa, property_tax ):
+def Funcao_01( area, rooms, bathroom, parking_spaces, floor, animal, furniture, hoa, property_tax ):
     """ 
     Função pega os dados da url e faz as previsões
     params = parâmetros passados na url
     modelo.predict(params)
     return o valor da previsão 
     """
+    Lista = [
+        float(area), float(rooms), float(bathroom), float(parking_spaces), 
+        float(floor), float(animal), float(furniture), float(hoa), float(property_tax)
+    ]
+    
     try:
-        previsao = modelo.predict()
-        return {'Valor_Aluguel' : previsao }
+        previsao = modelo.predict( [Lista] )
+        return {'Valor_Aluguel' : str(previsao) }
     except:
             return {'Aviso' : 'Ocorreu um erro! Passe os parâmetros <area>;<rooms>;<bathroom>;<parking_spaces>;<floor>;<animal>;<furniture>;<hoa>;<property_tax>>'}
     
--- Conexao_com_API.py
+++ Conexao_com_API.py
@@ -1,7 +1,7 @@
 import requests
 
 # Local da URL
-url = 'http://127.0.0.1:5000/API_Preditivo'
+url = 'http://127.0.0.1:5000/API_Preditivo/25;1;1;0;0;0;0;0;84'
 
 consulta = requests.get( url )
 
